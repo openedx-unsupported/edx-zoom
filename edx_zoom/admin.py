80b87b746638014010f1694d611a2c98a78b1b76
Dave St.Germain <dstgermain@edx.org>
2019-08-08 10:12:25 -0400

    Add LaunchLog to record access of each Zoom XBlock

--- edx_zoom/admin.py
+++ edx_zoom/admin.py
@@ -1,7 +1,17 @@
+"""
+Django Admin for edx-zoom
+"""
 from __future__ import absolute_import, unicode_literals
 
 from django.contrib import admin
 
-from .models import LTICredential
+from .models import LaunchLog, LTICredential
+
 
 admin.site.register(LTICredential)
+
+
+@admin.register(LaunchLog)
+class LaunchLogAdmin(admin.ModelAdmin):
+    readonly_fields = ('first_access', 'last_access', 'user')
+    list_display = ('id',  'user', 'course_id', 'location', 'last_access')
--- edx_zoom/models.py
+++ edx_zoom/models.py
@@ -1,9 +1,15 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
-from opaque_keys.edx.django.models import CourseKeyField
+from django.contrib.auth import get_user_model
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.timezone import now
+
+from opaque_keys.edx.django.models import CourseKeyField, UsageKeyField
+from opaque_keys.edx.keys import UsageKey
 
 
+@python_2_unicode_compatible
 class LTICredential(models.Model):
     course_id = CourseKeyField(max_length=255, unique=True)
     key = models.CharField(max_length=255)
@@ -12,3 +18,34 @@ class LTICredential(models.Model):
 
     def __str__(self):
         return 'LTI credential for %s' % self.course_id
+
+
+@python_2_unicode_compatible
+class LaunchLog(models.Model):
+    """
+    Records first/last user access to each Zoom XBlocks
+    """
+    user = models.ForeignKey(get_user_model(), null=True)
+    course_id = CourseKeyField(max_length=255, db_index=True)
+    location = UsageKeyField(max_length=255)
+    managed = models.BooleanField(db_index=True, default=False)
+    first_access = models.DateTimeField(auto_now_add=True, db_index=True)
+    last_access = models.DateTimeField(auto_now=True, db_index=True)
+
+    class Meta:
+        unique_together = ('user', 'location')
+
+    @classmethod
+    def update(cls, user_id, usage_key, managed):
+        obj, created = cls.objects.get_or_create(
+            user_id=user_id,
+            location=usage_key,
+            course_id=usage_key.course_key, defaults={'managed': managed})
+        if not created:
+            obj.last_access = now()
+            obj.managed = managed
+            obj.save()
+        return obj
+
+    def __str__(self):
+        return "{} visited {} on {}".format(self.user_id, self.location, self.last_access)
