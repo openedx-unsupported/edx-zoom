2bd7b2fca39f7972f5398b49f09bad0323a6bde7
Eric Herrera <eric.herrera@edunext.co>
2020-05-02 09:07:46 -0500

    Bump version. Add requirements files complying OEP-18. Add make upgrade instruction. Update setup.py install_requirements definition. Update travis file using new requirements. Add python 3.8. Remove old requirement files. Update openedx.yml OEPs list.

--- setup.py
+++ setup.py
@@ -19,19 +19,36 @@ def package_data(pkg, roots):
 
     return {pkg: data}
 
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
 
 setup(
     name='edx-zoom',
-    version='1.6',
+    version='1.7',
     description='This XBlock implements the LTI interface for Zoom video conferencing.',
     packages=[
         'edx_zoom',
     ],
-    install_requires=[
-        'edx-opaque-keys',
-        'django',
-        'lti_consumer-xblock',
-    ],
+    install_requires=load_requirements('requirements/base.in'),
     entry_points={
         'xblock.v1': [
             'edx_zoom = edx_zoom:ZoomXBlock',
